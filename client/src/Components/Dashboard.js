8b4bd28f6abc140149a02ac97b8a1a2683ddff78
Sai Hemanth B <saihemanth9019@gmail.com>
2020-06-28 19:10:22 +0530

    Add Dashboard UI

--- client/src/Components/Dashboard.js
+++ client/src/Components/Dashboard.js
@@ -1,9 +1,27 @@
 import React from 'react';
 import '../App.css';
+import {Button, Row} from 'react-bootstrap';
 
 function Dashboard() {
     return (
-        <div className={'dashboard'}></div>
+        <div className={'container dashboard'}>
+            <Row className={'title-row'}>
+                <div className={'inner-title-row'}>
+                    <b>Type Fighter</b>
+                    <p>Check out on&nbsp;
+                        <a
+                            target={'_blank'}
+                            href={'https://github.com/shb9019/type-fighter'}
+                            rel="noopener noreferrer">
+                        Github
+                        </a>
+                    </p>
+                </div>
+            </Row>
+            <Row className={'match-button-row'}>
+                <Button variant={'primary'} className={'match-button'}>Start Match</Button>
+            </Row>
+        </div>
     );
 }
 
